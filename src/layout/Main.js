5a3788afcc7b00cf67921f70498ee98bf036f790
rokib2000 <rokib0w1@gmail.com>
2022-10-20 15:57:57 +0600

    route setup

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,7 +1,33 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Header from "../Pages/Shared/Header/Header";
+import Footer from "../Pages/Shared/Footer/Footer";
+import LeftSideNav from "../Pages/Shared/LeftSideNav/LeftSideNav";
+import RightSideNav from "../Pages/Shared/RightSideNav/RightSideNav";
+import { Outlet } from "react-router-dom";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Container>
+        <Row>
+          <Col lg="2">
+            <LeftSideNav></LeftSideNav>
+          </Col>
+          <Col lg="7">
+            <Outlet></Outlet>
+          </Col>
+          <Col lg="3">
+            <RightSideNav></RightSideNav>
+          </Col>
+        </Row>
+      </Container>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
