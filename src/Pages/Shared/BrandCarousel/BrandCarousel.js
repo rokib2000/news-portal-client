41d650a3ec8153365f3d89ffceb1bd6eeeb2aff6
rokib2000 <rokib0w1@gmail.com>
2022-10-20 15:21:59 +0600

    project setup

---
+++ src/Pages/Shared/BrandCarousel/BrandCarousel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BrandCarousel = () => {
+  return (
+    <div>
+      <h2>Brand Carousel</h2>
+    </div>
+  );
+};
+
+export default BrandCarousel;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAyKX9TgfrtQ3xmamO6wEzNsIYASKth47A",
+  authDomain: "news-portal-client-53113.firebaseapp.com",
+  projectId: "news-portal-client-53113",
+  storageBucket: "news-portal-client-53113.appspot.com",
+  messagingSenderId: "934291218203",
+  appId: "1:934291218203:web:b9b1d2bb5719f694babdbe",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
