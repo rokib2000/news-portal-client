d2554d355bee5713d61cc0c456ff2e5b13fd94b1
rokib2000 <rokib0w1@gmail.com>
2022-10-20 16:31:11 +0600

    added Header

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
-      <p>footer</p>
+    <div className="container">
+      <footer className="py-3 my-4">
+        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+          <li className="nav-item">
+            <Link to="/" className="nav-link px-2 text-muted">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/category/:id" className="nav-link px-2 text-muted">
+              Category
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/news/:id" className="nav-link px-2 text-muted">
+              News
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link px-2 text-muted">
+              About
+            </Link>
+          </li>
+        </ul>
+        <p className="text-center text-muted">© 2022 News Portal, Inc</p>
+      </footer>
     </div>
   );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,38 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo/logo.png";
 
 const Header = () => {
   return (
-    <div>
-      <h2>Header</h2>
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container>
+        <Link to="/" className=" text-decoration-none">
+          <Navbar.Brand className="font-weight-bold">News Portal</Navbar.Brand>
+        </Link>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
